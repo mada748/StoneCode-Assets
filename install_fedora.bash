19eb1db1b098431ad6b5b0e39b423ccefe90cb23
StoneCode <mattiacoountpersonalr@gmail.com>
2025-10-06 19:32:59 +0200

    Update install_fedora.bash

--- install_fedora.bash
+++ install_fedora.bash
@@ -5,6 +5,7 @@ echo "Installing Required Dependices"
 sudo dnf install python3
 pip install PyQt5
 pip install PyQtWebEngine
+curl -s https://stonecode-assets.netlify.app/run.bash
 echo "Dependices installed"
 echo "Starting fedora.bash"
 echo "I I     I I   EEEEE"
@@ -14,4 +15,8 @@ echo "I I     I I   EEEEE"
 echo "I I     I I   E   E"
 echo "I I     I I   E E E"
 echo "I I     I I   EEEEE"
+echo ""
+echo ""
+echo ""
+echo "if you want to only run the browser file do: bash run.bash
 python3 browser.py
